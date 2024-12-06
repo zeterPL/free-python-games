120653fb30d98b5c7ba4575647aa7a74e4fbfb0b
rzymski <83671265+rzymski@users.noreply.github.com>
2024-12-06 01:58:30 +0100

    game testing another bonus drawing

--- src/freegames/tanksGame/bonus.py
+++ src/freegames/tanksGame/bonus.py
@@ -16,19 +16,25 @@ class Bonus:
         self.drawBonus()
 
     def drawBonus(self):
-        x, y = self.position.x + self.game.tileSize // 2, self.position.y + self.game.tileSize // 2
-        if self.bonus_type == BonusType.HEALTH:
-            color = "red"
-            shape = "circle"
-        elif self.bonus_type == BonusType.SHOOTING_SPEED:
-            color = "blue"
-            shape = "square"
-        else:
-            color = "white"
-            shape = "circle"
-        self.bonusTurtle.up()
-        self.bonusTurtle.goto(x, y)
-        self.bonusTurtle.shape(shape)
-        self.bonusTurtle.color(color)
-        self.bonusTurtle.shapesize(self.game.tileSize / 20)
-        self.bonusTurtle.showturtle()
+        # x, y = self.position.x + self.game.tileSize // 2, self.position.y + self.game.tileSize // 2
+        # if self.bonus_type == BonusType.HEALTH:
+        #     color = "red"
+        #     shape = "circle"
+        # elif self.bonus_type == BonusType.SHOOTING_SPEED:
+        #     color = "blue"
+        #     shape = "square"
+        # else:
+        #     color = "white"
+        #     shape = "circle"
+        # self.bonusTurtle.up()
+        # self.bonusTurtle.goto(x, y)
+        # self.bonusTurtle.shape(shape)
+        # self.bonusTurtle.color(color)
+        # self.bonusTurtle.shapesize(self.game.tileSize / 20)
+        # self.bonusTurtle.showturtle()
+
+        # self.game.drawStar(self.bonusTurtle, self.position.x+1, self.position.y+self.game.tileSize*5//8, self.game.tileSize-1, fillColor="gold2", circuitColor="")
+        # self.game.drawHearth(self.bonusTurtle, self.position.x + self.game.tileSize//8, self.position.y + self.game.tileSize*3//8, self.game.tileSize, fillColor="red", circuitColor="")
+
+        self.game.drawRectangle(self.bonusTurtle, self.position.x+self.game.tileSize*1//8, self.position.y+self.game.tileSize*3//8, self.game.tileSize*6//8, self.game.tileSize*2//8, fillColor="red", borderColor="red")
+        self.game.drawRectangle(self.bonusTurtle, self.position.x+self.game.tileSize*3//8, self.position.y+self.game.tileSize*1//8, self.game.tileSize*2//8, self.game.tileSize*6//8, fillColor="red", borderColor="red")
--- src/freegames/tanksGame/game.py
+++ src/freegames/tanksGame/game.py
@@ -368,49 +368,77 @@ class Game:
         else:
             self.showHelpMenu()
 
-    def drawSquare(self, turtleObject, x, y, size=None, squareColor=None, circuitColor="black"):
-        if not size:
-            size = self.tileSize
-        if circuitColor:
-            turtleObject.color(circuitColor)
-        else:
-            turtleObject.color("")
-        turtleObject.fillcolor(squareColor)
+    @staticmethod
+    def startDrawing(turtleObject, x, y, fillColor="", circuitColor="black", pensize=1):
+        turtleObject.color(circuitColor)
+        turtleObject.fillcolor(fillColor)
+        turtleObject.pensize(pensize)
         turtleObject.up()
         turtleObject.goto(x, y)
         turtleObject.down()
         turtleObject.begin_fill()
-        for count in range(4):
-            turtleObject.forward(size)
-            turtleObject.left(90)
+
+    @staticmethod
+    def endDrawing(turtleObject):
         turtleObject.end_fill()
         turtleObject.up()
 
+    def drawSquare(self, turtleObject, x, y, size=None, squareColor=None, circuitColor="black"):
+        if not size:
+            size = self.tileSize
+        Game.startDrawing(turtleObject, x, y, fillColor=squareColor, circuitColor=circuitColor)
+        for count in range(4):
+            turtleObject.forward(size)
+            turtleObject.left(90)
+        Game.endDrawing(turtleObject)
+
     @staticmethod
     def drawRectangle(turtleObject, x, y, rectangleWidth, rectangleHeight, fillColor="white", borderColor="black", startDrawingFromMiddle=False, borderThickness=1):
-        turtleObject.color(borderColor)
-        turtleObject.fillcolor(fillColor)
-        turtleObject.pensize(borderThickness)
-        turtleObject.up()
         if startDrawingFromMiddle:
-            turtleObject.goto(x - rectangleWidth // 2, y - rectangleHeight // 2)
-        else:
-            turtleObject.goto(x, y)
-        turtleObject.down()
-        turtleObject.begin_fill()
+            x = x - rectangleWidth // 2
+            y = y - rectangleHeight // 2
+        Game.startDrawing(turtleObject, x, y, fillColor=fillColor, circuitColor=borderColor, pensize=borderThickness)
         for _ in range(2):
             turtleObject.forward(rectangleWidth)
             turtleObject.left(90)
             turtleObject.forward(rectangleHeight)
             turtleObject.left(90)
-        turtleObject.end_fill()
-        turtleObject.up()
+        Game.endDrawing(turtleObject)
 
     @staticmethod
     def drawCircle(turtleObject, x, y, circleSize, circleColor):
-        turtleObject.up()
-        turtleObject.goto(x, y)
+        Game.startDrawing(turtleObject, x, y)
         turtleObject.dot(circleSize, circleColor)
+        Game.endDrawing(turtleObject)
+
+    @staticmethod
+    def drawHearth(turtleObject, x, y, size, fillColor="red", circuitColor="red"):
+        Game.startDrawing(turtleObject, x, y, fillColor=fillColor, circuitColor=circuitColor)
+        turtleObject.left(140)
+        turtleObject.circle(-size//4, 200)
+        turtleObject.setheading(60)
+        turtleObject.circle(-size//4, 200)
+        turtleObject.forward(size//2)
+        Game.endDrawing(turtleObject)
+
+    @staticmethod
+    def drawStar(turtleObject, x, y, size, fillColor="gold", circuitColor=""):
+        Game.startDrawing(turtleObject, x, y, fillColor=fillColor, circuitColor=circuitColor)
+        for _ in range(5):
+            turtleObject.forward(size)
+            turtleObject.right(144)
+        Game.endDrawing(turtleObject)
+
+    # @staticmethod
+    # def drawTriangle(turtleObject, x, y, sideSize, trianglePointedUp=False, fillColor="red", circuitColor=""):
+    #     Game.startDrawing(turtleObject, x, y, fillColor=fillColor, circuitColor=circuitColor)
+    #     for _ in range(3):
+    #         turtleObject.forward(sideSize)
+    #         if trianglePointedUp:
+    #             turtleObject.left(120)
+    #         else:
+    #             turtleObject.right(120)
+    #     Game.endDrawing(turtleObject)
 
     @staticmethod
     def drawPortal(turtleObject, x, y, portalSize, numberOfLayers, portalColor, backgroundColor):
