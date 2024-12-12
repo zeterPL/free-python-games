e6b58dd1a2f1e0f62c946127d287a654289eb524
rzymski <83671265+rzymski@users.noreply.github.com>
2024-12-12 07:17:57 +0100

    demo game in README.md back to gif

--- src/freegames/tanksGame/README.md
+++ src/freegames/tanksGame/README.md
@@ -17,7 +17,7 @@ ___
 # Opis gry
 Gra w czołgi, w której celem jest zniszczenie wszystkich wrogich czołgów.</br>
 W zależności od wybranego trybu gry, gracz musi zniszczyć wszystkie wrogie czołgi komputera lub wrogi czołg innego gracza. 
-![Demo game](files/readmeFiles/demo.mp4)
+![Demo game](files/readmeFiles/demo.gif)
 
 # Funkcjonalność
 **Gra w czołgi**
--- src/freegames/tanksGame/tank.py
+++ src/freegames/tanksGame/tank.py
@@ -157,7 +157,7 @@ class Tank:
         if self.game.tiles[self.game.getTileIndexFromPoint(self.position + int(self.game.tileSize * 0.4))] != Tile.FOREST.value:
             Draw.drawReloadBar(self)
 
-    @Utils.debugPrintActualHpSituation
+    # @Utils.debugPrintActualHpSituation
     def takeDamage(self, amount, reason):
         if not self.destroyed and not self.indestructible:
             self.hp -= amount
